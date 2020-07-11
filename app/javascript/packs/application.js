8aae8280e03f80eee26e5bf47c110c4203b61fbb
Chris Sharp Rails course <sharpchr@gmail.com>
2020-07-11 18:48:17 +0100

    Bootstrap imported try again

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-@import "bootstrap/scss/bootstrap"
+import "bootstrap"
+
+
+
 
