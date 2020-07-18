976d0e71249945a1abf11a0552504efe212815fc
Chris Sharp Rails course <sharpchr@gmail.com>
2020-07-18 14:53:47 +0100

    Styling changes 18/07

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require jquery
-require jquery-ui
-require popper
-require bootstrap-sprockets
-require rails-ujs
-require activestorage
-require turbolinks
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
 require_tree .
 
 
