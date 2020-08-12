56b02172e8b8ccd76116628a911f5bf90746d8a0
Chris Sharp Rails course <sharpchr@gmail.com>
2020-08-12 15:21:35 +0100

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
-	before_action :set_user, only: [:show, :edit, :update]
+	before_action :set_user, only: [:show, :edit, :update, :destroy]																																				
 	before_action :require_user, only: [:edit, :update]
-	before_action :require_same_user, only: [:edit, :update]		
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	
 	def show
 	@articles=@user.articles.paginate(page: params[:page], per_page: 5)
 	end
@@ -37,18 +38,28 @@ class UsersController < ApplicationController
 		render 'new'
 		end
 	end
-
+	
+	
+	def destroy
+	@user.destroy
+	session[:user_id] = nil if @user == current_user
+	flash[:notice] = "Account and all associated articles successfully deleted"
+	redirect_to articles_path																																							
+	end
+	
+	
 	private
 	def user_params
 	params.require(:user).permit(:username, :email, :password)
 	end
+		
 	def set_user
 	@user = User.find(params[:id])
 	end
+	
 	def require_same_user
-	 
-		if current_user != @article_user
-		flash[:alert] = "You can only edit or delete your own profile"
+	 	if current_user != @user && !current_user.admin?
+		flash[:alert] = "You can only edit or delete your own account"
 		redirect_to @user
 		end
 	 end
